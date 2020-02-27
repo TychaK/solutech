3af144133e8299a790e53f6d7c3d0a2ea89fc8b5
TychaK <kelvinkibugi@gmail.com>
2020-02-27 18:57:39 +0300

    Modified api.php routes to accept only one parameter for productdetails and supplierproducts

--- routes/web.php
+++ routes/web.php
@@ -38,3 +38,57 @@ Route::get('dashboard/orders/update', function () {
 Route::get('dashboard/orders/remove', function () {
     return view('admin.remove-orders');
 });
+
+Route::get('/dashboard/products', function () {
+    return view('admin.products');
+});
+Route::get('dashboard/products/create', function () {
+    return view('admin.create-products');
+});
+Route::get('dashboard/products/update', function () {
+    return view('admin.update-products');
+});
+Route::get('dashboard/products/remove', function () {
+    return view('admin.remove-products');
+});
+
+Route::get('/dashboard/suppliers', function () {
+    return view('admin.suppliers');
+});
+Route::get('dashboard/suppliers/create', function () {
+    return view('admin.create-suppliers');
+});
+Route::get('dashboard/suppliers/update', function () {
+    return view('admin.update-suppliers');
+});
+Route::get('dashboard/suppliers/remove', function () {
+    return view('admin.remove-suppliers');
+});
+
+
+Route::get('/dashboard/order_details', function () {
+    return view('admin.order-details');
+});
+Route::get('dashboard/order_details/create', function () {
+    return view('admin.create-order-details');
+});
+Route::get('dashboard/order_details/update', function () {
+    return view('admin.update-order-details');
+});
+Route::get('dashboard/order_details/remove', function () {
+    return view('admin.remove-order-details');
+});
+
+
+Route::get('/dashboard/supplier_products', function () {
+    return view('admin.supplier-products');
+});
+Route::get('dashboard/supplier_products/create', function () {
+    return view('admin.create-supplier-products');
+});
+Route::get('dashboard/supplier_products/update', function () {
+    return view('admin.update-supplier-products');
+});
+Route::get('dashboard/supplier_products/remove', function () {
+    return view('admin.remove-supplier-products');
+});
