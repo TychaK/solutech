f822f207e6441d6aa715c183bf6e1ddbd82ee48c
TychaK <kelvinkibugi@gmail.com>
2020-03-05 13:00:10 +0300

    Modified Models Order.php & Supplier.php with Laravel Relationships.(hasMany)

--- app/Order.php
+++ app/Order.php
@@ -9,4 +9,11 @@ class Order extends Model
     //
     protected $table = "orders";
     protected $fillable = ['order_number'];
+
+    // relations has many order details...
+
+    public function orderDetail()
+    {
+        return $this->hasMany('App\OrderDetail', 'order_id');
+    }
 }
--- app/Supplier.php
+++ app/Supplier.php
@@ -9,4 +9,9 @@ class Supplier extends Model
     //
     protected $table = "suppliers";
     protected $fillable = ['name'];
+
+    public function supplierProduct()
+    {
+        return $this->hasMany('\App\SupplierProduct', 'supply_id');
+    }
 }
